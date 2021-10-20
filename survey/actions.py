a47ea19ae54e80ffb42b33d704137a3cf116a7a4
Rantaharju Jarno <jarno.rantaharju@aalto.fi>
2021-10-20 11:47:27 +0300

    Add an action to copy a survey

--- survey/actions.py
+++ survey/actions.py
@@ -1,6 +1,8 @@
 from django.utils.translation import gettext_lazy as _
 from django.utils.translation import ungettext
 
+from survey.models import Category, Question
+
 
 def make_published(modeladmin, request, queryset):
     """
@@ -19,3 +21,46 @@ def make_published(modeladmin, request, queryset):
 
 
 make_published.short_description = _("Mark selected surveys as published")
+
+
+def copy_survey(modeladmin, request, queryset):
+    for survey in queryset:
+        # Note the id
+        original_id = survey.pk
+
+        # Create a copy of the survey object by saving it with a new pk
+        survey.pk = None
+        survey.name = survey.name + " Copy"
+        survey.save()
+
+        # Note new survey id
+        copy_pk = survey.pk
+
+        # Now copy all categories connected to the original survey with the new
+        # survey id
+        categories_map = {}
+        for category in Category.objects.all():
+            if category.survey.pk == original_id:
+                # This category is linked to the survey, so create a copy.
+                # Note the old and new ids in categories_map, this is needed
+                # when copying questions
+                original_pk = category.pk
+                category.pk = None
+                category.survey = survey
+                category.save()
+                categories_map[original_pk] = category
+
+        # Then copy questions connected to this survey
+        for question in Question.objects.all():
+            if question.survey.pk == original_id:
+                # This question is linked to the survey, so create a copy.
+                # Note the old and new ids in categories_map, this is needed
+                # when copying questions
+                question.pk = None
+                question.survey = survey
+                if question.category is not None:
+                    question.category = categories_map[question.category.pk]
+                question.save()
+
+
+copy_survey.short_description = _("Create copies of selected surveys")
