438aa037be655a1e0960c9e55515eb2117911035
Rantaharju Jarno <jarno.rantaharju@aalto.fi>
2026-09-01 15:34:04 +0300

    Add migration

---
+++ survey/migrations/0018_add_scale_0_10_type.py
@@ -0,0 +1,32 @@
+from django.db import migrations, models
+
+
+class Migration(migrations.Migration):
+    dependencies = [("survey", "0017_questioncondition")]
+
+    operations = [
+        migrations.AlterField(
+            model_name="question",
+            name="type",
+            field=models.CharField(
+                choices=[
+                    ("text", "text (multiple line)"),
+                    ("short-text", "short text (one line)"),
+                    ("radio", "radio"),
+                    ("select", "select"),
+                    ("select-multiple", "Select Multiple"),
+                    ("select_image", "Select Image"),
+                    ("integer", "integer"),
+                    ("float", "float"),
+                    ("date", "date"),
+                    ("likert-5", "5-point likert"),
+                    ("scale-0-10", "0-10 scale"),
+                    ("time", "time"),
+                    ("datetime", "date and time"),
+                ],
+                default="text",
+                max_length=200,
+                verbose_name="Type",
+            ),
+        )
+    ]
